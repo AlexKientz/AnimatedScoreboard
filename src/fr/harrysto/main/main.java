b88e884fef9020ef0a316c8dcf98ea4c0e8806cc
Harrysto67 <alexkientz6@gmail.Com>
2021-10-27 18:46:06 +0200

    Final version

--- src/fr/harrysto/main/main.java
+++ src/fr/harrysto/main/main.java
@@ -17,37 +17,47 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class main extends JavaPlugin implements Listener {
     private boolean titlechanged = false;
 
+    @Override
     public void onEnable() {
         saveDefaultConfig();
         File configFile = new File("plugins/Julien/config.yml");
         YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(configFile);
-        final PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
-        (new BukkitRunnable() {
+        //this.getServer().getPluginManager().registerEvents(this, this);
+        PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
+        new BukkitRunnable() {
+
+            @Override
             public void run() {
                 try {
                     Field a = packet.getClass().getDeclaredField("a");
                     a.setAccessible(true);
                     Field b = packet.getClass().getDeclaredField("b");
                     b.setAccessible(true);
-                    Object header1 = new ChatComponentText(main.this.getConfig().getString("header1"));
-                    Object header2 = new ChatComponentText(main.this.getConfig().getString("header2"));
-                    Object footer = new ChatComponentText(String.valueOf(main.this.getConfig().getString("online")) + Bukkit.getServer().getOnlinePlayers().size());
-                    if (main.this.titlechanged) {
+
+                    Object header1 = new ChatComponentText(getConfig().getString("header1"));
+                    Object header2 = new ChatComponentText(getConfig().getString("header2"));
+
+                    Object footer = new ChatComponentText(getConfig().getString("online") + Bukkit.getServer().getOnlinePlayers().size());
+                    if (titlechanged) {
                         a.set(packet, header2);
-                        main.this.titlechanged = false;
+                        titlechanged = false;
+
                     } else {
                         a.set(packet, header1);
-                        main.this.titlechanged = true;
+                        titlechanged = true;
                     }
                     b.set(packet, footer);
-                    if (Bukkit.getOnlinePlayers().size() == 0)
-                        return;
-                    for (Player player : Bukkit.getOnlinePlayers())
-                        (((CraftPlayer)player).getHandle()).playerConnection.sendPacket((Packet)packet);
-                } catch (NoSuchFieldException|IllegalAccessException e) {
+
+                    if (Bukkit.getOnlinePlayers().size() == 0) return;
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+                    }
+
+                } catch (NoSuchFieldException | IllegalAccessException e) {
                     e.printStackTrace();
                 }
             }
-        }).runTaskTimer((Plugin)this, 0L, 20L);
+        }.runTaskTimer(this, 0, 20);
     }
+
 }
